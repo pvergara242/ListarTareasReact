11d74886f39f285f886bfbfa040840f64e51cf8b
Paola Andrea Vergara Lopez <Paola@MacBook-Pro-de-Hansel.local>
2020-12-21 22:45:00 -0500

    mi primera aplicacion en React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
+import React, {Component} from 'react'
 import './App.css';
+import {todos} from "./todos.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+  }
+  render(){
+      const todos = this.state.todos.map((todo, i)=>{
+      return(
+        <div className="col-md-4">
+        <div className="card mt-5">
+          <div className="card-header">
+            <h2>{todo.title}</h2>
+            <span className="badge badge-pill badge-danger ml-2">
+              {todo.prioridad}
+            </span>
+          </div>
+          <div className="card-body">
+            <h3>{todo.descripcion}</h3>
+            <p>{todo.responsable}</p>
+          </div>
+        </div>
+        </div>
+      )
+    })
+    return (
+      <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+          <a  className="text-white">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+                {this.state.todos.length}
+            </span>
+          </a>
+      </nav>
+      <div className="container">
+        <div className="row mt-4">
+              { todos }
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
-
 export default App;
